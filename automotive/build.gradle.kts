38577ed11d8dc9d816f3484dde8839bf6287a364
Amanda Briena <55834808+amandabriena@users.noreply.github.com>
2025-05-11 11:58:42 -0300

    Adicionado dependencias de teste

--- automotive/build.gradle.kts
+++ automotive/build.gradle.kts
@@ -66,6 +66,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("org.mockito:mockito-core:5.5.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
+
 
     // Navigation
     val nav_version = "2.8.9"
