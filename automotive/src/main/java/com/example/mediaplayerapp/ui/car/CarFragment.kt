7a0e2c21f7829b351ee26e173d382618e0f45a4c
Aron Caiuá <71573532+Aron-brito@users.noreply.github.com>
2025-07-25 13:46:55 -0300

    Add new fragment

---
+++ automotive/src/main/java/com/example/mediaplayerapp/ui/car/CarFragment.kt
@@ -0,0 +1,34 @@
+package com.example.mediaplayerapp.ui.car
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
+import com.example.mediaplayerapp.R
+
+
+class CarFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+
+        return inflater.inflate(R.layout.fragment_car, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val backButton: ImageView = view.findViewById(R.id.backButton)
+        backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+    }
+}
